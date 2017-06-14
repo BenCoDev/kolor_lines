7be537cb55578e6c3c615f0d623c1288702ca84f
Bencodev <benoit.cotte@essec.edu>
2017-06-14 12:43:52 +0900

    Add fetchAlignments, mergeDirectionAligments and listNeighbours methods to Board

--- Board.java
+++ Board.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Represents the Board for Kolor Lines games
  *
@@ -19,6 +22,129 @@ public class Board {
 
     public void setSquare(Position pos){
         this.squares[pos.getAbs()][pos.getOrd()] = new Square(pos);
+    }
+
+    public void setSquare(Position pos, Color color){
+        this.squares[pos.getAbs()][pos.getOrd()] = new Square(pos, color);
+    }
+
+    /**
+     * Check if Board has alignment starting a Position
+     *
+     * Direction is optional
+     */
+    public LinkedList<Square>[] fetchAlignments(Position pos, LinkedList<Square>[] alignmentsByDirection){
+        //  Means it is the first square
+        //  If no directions
+        //  List neighbours
+        Square curSquare = getSquare(pos);
+        Square[] neighbours;
+
+        neighbours = this.listNeighbours(pos);
+
+        for (int i = 0; i < neighbours.length; i++){
+
+            if (neighbours[i] != null &&
+                    (neighbours[i].getColor() == curSquare.getColor() ||
+                            (neighbours[i].getColor() == Color.RAINBOW || curSquare.getColor() == Color.RAINBOW))){
+                try {
+                    alignmentsByDirection[i].add(neighbours[i]);
+                }
+                catch (java.lang.NullPointerException e){
+                    alignmentsByDirection[i] = new LinkedList<Square>();
+                    alignmentsByDirection[i].add(neighbours[i]);
+                }
+
+                fetchAlignments(neighbours[i].getPosition(), alignmentsByDirection, Board.Direction.values()[i]);
+            }
+        }
+
+        return alignmentsByDirection;
+    }
+
+    public LinkedList<Square>[] fetchAlignments(Position pos, LinkedList<Square>[] alignmentsByDirection, Direction direction){
+
+        // TODO: Describe alignmensByDirection
+        // Same structure with same 8 slots
+        Square curSquare = getSquare(pos);
+
+        Square nextSquare;
+        try {
+            nextSquare = getSquare(pos.getNextPosition(direction));
+        } catch (Exception e) {
+            System.out.println("No more squares on this direction");
+            nextSquare = null;
+        }
+
+        if (nextSquare != null) {
+            if (curSquare.getColor() == nextSquare.getColor() ||
+                    (nextSquare.getColor() == Color.RAINBOW || curSquare.getColor() == Color.RAINBOW)) {
+                alignmentsByDirection[direction.ordinal()].add(nextSquare);
+            }
+
+            fetchAlignments(nextSquare.getPosition(), alignmentsByDirection, direction);
+        }
+
+        return alignmentsByDirection;
+    }
+
+    /**
+     * mergedAlignmentsByDirection
+     * 0: NWSE
+     * 1: WE
+     * 2: SWNE
+     * 3: SN
+     * @param alignmentsByDirection
+     * @return
+     */
+    public static LinkedList<Square>[] mergeDirectionAligments(LinkedList<Square>[] alignmentsByDirection){
+        LinkedList<Square>[] mergedAlignmentsByDirection = new LinkedList[4];
+
+        // Make more sense to decrement
+        // i from 7 to 4 included
+        int outputArrayIndex;
+        for (int i = alignmentsByDirection.length - 1; i >= mergedAlignmentsByDirection.length; i--){
+            outputArrayIndex = -(i - (alignmentsByDirection.length - 1));
+
+            if (alignmentsByDirection[i] != null){
+                try {
+                    mergedAlignmentsByDirection[outputArrayIndex].addAll(alignmentsByDirection[i]);
+                }
+                catch (java.lang.NullPointerException e){
+                    mergedAlignmentsByDirection[outputArrayIndex] = new LinkedList<Square>();
+                    mergedAlignmentsByDirection[outputArrayIndex].addAll(alignmentsByDirection[i]);
+                }
+            }
+
+            if (alignmentsByDirection[i - mergedAlignmentsByDirection.length] != null){
+                try {
+                    mergedAlignmentsByDirection[outputArrayIndex].addAll(alignmentsByDirection[i - mergedAlignmentsByDirection.length]);
+                }
+                catch (java.lang.NullPointerException e){
+                    mergedAlignmentsByDirection[outputArrayIndex] = new LinkedList<Square>();
+                    mergedAlignmentsByDirection[outputArrayIndex].addAll(alignmentsByDirection[i - mergedAlignmentsByDirection.length]);
+                }
+            }
+        }
+        return mergedAlignmentsByDirection;
+    }
+
+    public Square[] listNeighbours(Position pos){
+        Square[] neighbours = new Square[Board.Direction.values().length];
+
+        // Get N, NE, E, SE , S, SW, W, NW and append in this order; null if empty
+        for (int i = 0; i < Board.Direction.values().length; i ++){
+            Direction curDirection = Board.Direction.values()[i];
+            try {
+                neighbours[i] = this.getSquare(pos.getNextPosition(curDirection));
+            }
+            catch (Exception e) {
+                System.out.println("No more squares on this direction");
+                neighbours[i] = null;
+            }
+        }
+
+        return neighbours;
     }
 
     public void display(){
@@ -52,4 +178,6 @@ public class Board {
 
     private int size;
     private Square[][] squares;
+    public enum Direction { N, NE, E, SE, S, SW, W, NW };
+    private static int MIN_ALIGNMENT_LENGTH = 5;
 }
--- BoardTest.java
+++ BoardTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -48,6 +50,112 @@ public class BoardTest {
                 "         /         0\n" +
                 "         0" + String.format("%10s", b.getSquare(position).getColor()) + "\n"
         );
+    }
+
+    @Test
+    public void listNeighbours() throws Exception {
+//        TODO: refine
+        Board b = new Board(2);
+        Position originalPosition = new Position(0, 0);
+        b.setSquare(originalPosition);
+
+        Position position = new Position(1, 0);
+        b.listNeighbours(position);
+        b.display();
+    }
+
+    @Test
+    public void fetchAlignments_BasicCase() throws Exception {
+        Board b = new Board(2);
+        Position originalPosition = new Position(1, 1);
+        b.setSquare(originalPosition);
+
+        LinkedList<Square>[] alignmentsByDirection = new LinkedList[8];
+
+        assertArrayEquals(b.fetchAlignments(originalPosition, alignmentsByDirection), new LinkedList[8]);
+    }
+
+    @Test
+    public void fetchAlignments_MultipleDirectionAlignments() throws Exception {
+        Board b = new Board(3);
+
+        Position originalPosition = new Position(1, 1);
+        b.setSquare(originalPosition, Color.BLEU);
+
+        // Add NE with same color
+        Position NEPosition = new Position(2, 0);
+        b.setSquare(NEPosition, Color.BLEU);
+        Square squareNE = b.getSquare(NEPosition);
+
+        // Add E with different color
+        Position EPosition = new Position(2, 1);
+        b.setSquare(EPosition, Color.VERT);
+        Square squareE = b.getSquare(EPosition);
+
+        // Add SW with same color
+        Position SWPosition = new Position(0, 2);
+        b.setSquare(SWPosition, Color.BLEU);
+        Square squareSW = b.getSquare(SWPosition);
+
+
+        LinkedList<Square>[] alignmentsByDirection = new LinkedList[8];
+        LinkedList<Square>[] expectedArray = new LinkedList[8];
+
+        expectedArray[Board.Direction.valueOf("NE").ordinal()] = new LinkedList<Square>();
+        expectedArray[Board.Direction.valueOf("NE").ordinal()].add(squareNE);
+        expectedArray[Board.Direction.valueOf("SW").ordinal()] = new LinkedList<Square>();
+        expectedArray[Board.Direction.valueOf("SW").ordinal()].add(squareSW);
+
+        assertArrayEquals(expectedArray, b.fetchAlignments(originalPosition, alignmentsByDirection));
+    }
+
+    @Test
+    public void fetchAlignments_MultipleSquareAlignments() throws Exception {
+        Board b = new Board(3);
+
+        Position originalPosition = new Position(0, 0);
+        b.setSquare(originalPosition, Color.BLEU);
+
+        // Add nextPosition with same color
+        Position nextPosition = new Position(1, 1);
+        b.setSquare(nextPosition, Color.BLEU);
+        Square nextSquare = b.getSquare(nextPosition);
+
+        // Add nextNextPosition with same color
+        Position nextNextPosition = new Position(2, 2);
+        b.setSquare(nextNextPosition, Color.BLEU);
+        Square nextNextSquare = b.getSquare(nextNextPosition);
+
+        LinkedList<Square>[] alignmentsByDirection = new LinkedList[8];
+        LinkedList<Square>[] expectedArray = new LinkedList[8];
+
+        expectedArray[Board.Direction.valueOf("SE").ordinal()] = new LinkedList<Square>();
+        expectedArray[Board.Direction.valueOf("SE").ordinal()].add(nextSquare);
+        expectedArray[Board.Direction.valueOf("SE").ordinal()].add(nextNextSquare);
+
+        assertArrayEquals(expectedArray, b.fetchAlignments(originalPosition, alignmentsByDirection));
+    }
+
+    @Test
+    public void mergeDirectionAligments() throws Exception {
+        Square WSquare = new Square(new Position(0, 1));
+        Square ESquare = new Square(new Position(2, 1));
+
+        LinkedList<Square>[] alignmentsByDirection = new LinkedList[8];
+        alignmentsByDirection[Board.Direction.valueOf("W").ordinal()] = new LinkedList<Square>();
+        alignmentsByDirection[Board.Direction.valueOf("W").ordinal()].add(WSquare);
+
+        alignmentsByDirection[Board.Direction.valueOf("E").ordinal()] = new LinkedList<Square>();
+        alignmentsByDirection[Board.Direction.valueOf("E").ordinal()].add(ESquare);
+
+
+        LinkedList<Square>[] expectedArray = new LinkedList[4];
+        expectedArray[1] = new LinkedList<Square>();
+        expectedArray[1].add(WSquare);
+        expectedArray[1].add(ESquare);
+
+        assertArrayEquals(expectedArray, Board.mergeDirectionAligments(alignmentsByDirection));
+
     }
 
 }
