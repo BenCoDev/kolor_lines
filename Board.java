a4e4dd1903221426543c05dc05bc801832794144
Bencodev <benoit.cotte@essec.edu>
2017-06-15 11:35:14 +0900

    Refine docs

--- Board.java
+++ Board.java
@@ -38,6 +38,64 @@ public class Board {
         return true;
     }
 
+    /**
+     * Get Alignments bigger than given minimumLength which includes given position
+     *
+     * Will first compute all the alignments from given position according 8 directions
+     * Will then merge found alignments according 4 directions and including the square at given position
+     * Finally check length
+     *
+     * @param pos - Position - Position of the original point from where alignments are computed
+     * @param minimumLength - int - Minimum length needed for an alignment to be returned
+     * @return LinkedList - List of Lists of the squares in returned alignments
+     */
+    public LinkedList getAlignments(Position pos, int minimumLength){
+        LinkedList<Square>[] alignmentsByDirection = this.fetchAlignments(pos);
+        LinkedList<Square>[] mergedDirectionAlignments = Board.mergeAlignments(this.getSquare(pos), alignmentsByDirection);
+        LinkedList<LinkedList<Square>> minimumLengthAlignments = new LinkedList<LinkedList<Square>>();
+
+        for (LinkedList<Square> mergedDirectionAlignment : mergedDirectionAlignments) {
+            if (mergedDirectionAlignment != null){
+                if (Board.isAlignmentValid(mergedDirectionAlignment, minimumLength)) {
+                    minimumLengthAlignments.add(mergedDirectionAlignment);
+                }
+            }
+        }
+
+        return minimumLengthAlignments;
+    }
+
+    public void display(){
+        String formattedTable = "";
+
+        formattedTable += String.format("%10s", "/");
+        for (int colIndex=0; colIndex < this.squares[0].length; colIndex++){
+            formattedTable += String.format("%10s", colIndex);
+        }
+        formattedTable += "\n";
+
+        // Print row by row
+        for (int rowIndex=0; rowIndex < this.squares.length; rowIndex++){
+
+            for (int colIndex=0; colIndex < this.squares[rowIndex].length; colIndex++){
+                if (colIndex == 0){
+                    formattedTable += String.format("%10s", rowIndex);
+                }
+
+                if (Square.class.isInstance(this.squares[rowIndex][colIndex])){
+                    formattedTable += String.format("%10s", this.squares[rowIndex][colIndex].getColor());
+                }
+                else {
+                    formattedTable += String.format("%10s", " ");
+                }
+
+            }
+            formattedTable += "\n";
+        }
+
+        System.out.print(formattedTable);
+    }
+
     public Square getSquare(Position pos){
         return this.squares[pos.getOrd()][pos.getAbs()];
     }
@@ -58,14 +116,40 @@ public class Board {
     }
 
     /**
-     * Check if Board has alignment starting a Position
+     * List the squares surrounding a position
      *
-     * Direction is optional
+     * @param pos - Position - Position at which we want to list the neighbours
+     * @return - Square[] - List of squares for every direction listed
+     *         Given in the order defined by the Direction enum
+     *         null if empty
      */
-    private LinkedList<Square>[] fetchAlignments(Position pos, LinkedList<Square>[] alignmentsByDirection){
-        //  Means it is the first square
-        //  If no directions
-        //  List neighbours
+    private Square[] listNeighbours(Position pos){
+        Square[] neighbours = new Square[Board.Direction.values().length];
+
+        // Get N, NE, E, SE , S, SW, W, NW and append in this order; null if empty
+        for (int i = 0; i < Board.Direction.values().length; i ++){
+            Direction curDirection = Board.Direction.values()[i];
+            try {
+                neighbours[i] = this.getSquare(pos.getNextPosition(curDirection));
+            }
+            catch (Exception e) {
+                System.out.println("No more squares on this direction");
+                neighbours[i] = null;
+            }
+        }
+
+        return neighbours;
+    }
+
+    /**
+     * Fetch alignments given a position
+     *
+     * @param pos - Position - Position of the original point from where alignments are computed
+     * @return LinkedList - List of lists of aligned squares by direction
+     *                      Given in the order of the Direction enum
+     */
+    private LinkedList<Square>[] fetchAlignments(Position pos){
+        LinkedList<Square>[] alignmentsByDirection = new LinkedList[Direction.values().length];
         Square curSquare = getSquare(pos);
         Square[] neighbours;
 
@@ -76,6 +160,7 @@ public class Board {
             if (neighbours[i] != null &&
                     (neighbours[i].getColor() == curSquare.getColor() ||
                             (neighbours[i].getColor() == Color.RAINBOW || curSquare.getColor() == Color.RAINBOW))){
+
                 try {
                     alignmentsByDirection[i].add(neighbours[i]);
                 }
@@ -91,23 +176,34 @@ public class Board {
         return alignmentsByDirection;
     }
 
+    /**
+     * Fetch alignments given a position, an existing array of alignments by directions, and a given direction
+     *
+     * @param pos - Position - Position of the original point from where alignments are computed
+     * @param alignmentsByDirection - Array of LinkedList of Squares - Array of alignments by directions -
+     *                              will be extended in place
+     * @param direction - Direction
+     * @return alignmentsByDirection
+     */
     private LinkedList<Square>[] fetchAlignments(Position pos, LinkedList<Square>[] alignmentsByDirection, Direction direction){
-
-        // TODO: Describe alignmensByDirection
-        // Same structure with same 8 slots
         Square curSquare = getSquare(pos);
-
         Square nextSquare;
+
         try {
             nextSquare = getSquare(pos.getNextPosition(direction));
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             System.out.println("No more squares on this direction");
             nextSquare = null;
         }
 
         if (nextSquare != null) {
+            // FIXME: problem on ROUGE RAINBOW BLEU: Check curSquare
+            // If curSquare is rainbow go to previous until not rainbow
+            // TODO: cover with tests
+
             if (curSquare.getColor() == nextSquare.getColor() ||
-                    (nextSquare.getColor() == Color.RAINBOW || curSquare.getColor() == Color.RAINBOW)) {  // FIXME: problem on ROUGE RAINBOW BLEU
+                    (nextSquare.getColor() == Color.RAINBOW || curSquare.getColor() == Color.RAINBOW)) {
                 alignmentsByDirection[direction.ordinal()].add(nextSquare);
             }
 
@@ -115,30 +211,6 @@ public class Board {
         }
 
         return alignmentsByDirection;
-    }
-
-    public LinkedList getMinLengthAlignments(Position pos, int minimumLength){
-        LinkedList<Square>[] alignmentsByDirection = new LinkedList[8];
-        alignmentsByDirection = this.fetchAlignments(pos, alignmentsByDirection);
-
-        LinkedList<Square>[] mergedDirectionAlignments = Board.mergeDirectionAlignments(this.getSquare(pos), alignmentsByDirection);
-
-        LinkedList minimumLengthAlignments = new LinkedList();
-
-        for (LinkedList<Square> mergedDirectionAlignment : mergedDirectionAlignments) {
-            if (mergedDirectionAlignment != null){
-                if (Board.isAlignmentValid(mergedDirectionAlignment, minimumLength)) {
-                    minimumLengthAlignments.add(mergedDirectionAlignment);
-                }
-            }
-
-        }
-
-        return minimumLengthAlignments;
-    }
-
-    private static boolean isAlignmentValid(LinkedList<Square> alignment, int minimumLength) {
-        return (alignment.size() >= minimumLength);
     }
 
     /**
@@ -152,7 +224,7 @@ public class Board {
      * @param alignmentsByDirection
      * @return
      */
-    private static LinkedList<Square>[] mergeDirectionAlignments(Square curSquare, LinkedList<Square>[] alignmentsByDirection){
+    private static LinkedList<Square>[] mergeAlignments(Square curSquare, LinkedList<Square>[] alignmentsByDirection){
         LinkedList<Square>[] mergedAlignmentsByDirection = new LinkedList[4];
 
         // Make more sense to decrement
@@ -192,53 +264,8 @@ public class Board {
         return mergedAlignmentsByDirection;
     }
 
-    public Square[] listNeighbours(Position pos){
-        Square[] neighbours = new Square[Board.Direction.values().length];
-
-        // Get N, NE, E, SE , S, SW, W, NW and append in this order; null if empty
-        for (int i = 0; i < Board.Direction.values().length; i ++){
-            Direction curDirection = Board.Direction.values()[i];
-            try {
-                neighbours[i] = this.getSquare(pos.getNextPosition(curDirection));
-            }
-            catch (Exception e) {
-                System.out.println("No more squares on this direction");
-                neighbours[i] = null;
-            }
-        }
-
-        return neighbours;
-    }
-
-    public void display(){
-        String formattedTable = "";
-
-        formattedTable += String.format("%10s", "/");
-        for (int colIndex=0; colIndex < this.squares[0].length; colIndex++){
-            formattedTable += String.format("%10s", colIndex);
-        }
-        formattedTable += "\n";
-
-        // Print row by row
-        for (int rowIndex=0; rowIndex < this.squares.length; rowIndex++){
-
-            for (int colIndex=0; colIndex < this.squares[rowIndex].length; colIndex++){
-                if (colIndex == 0){
-                    formattedTable += String.format("%10s", rowIndex);
-                }
-
-                if (Square.class.isInstance(this.squares[rowIndex][colIndex])){
-                    formattedTable += String.format("%10s", this.squares[rowIndex][colIndex].getColor());
-                }
-                else {
-                    formattedTable += String.format("%10s", " ");
-                }
-
-            }
-            formattedTable += "\n";
-        }
-
-        System.out.print(formattedTable);
+    private static boolean isAlignmentValid(LinkedList<Square> alignment, int minimumLength) {
+        return (alignment.size() >= minimumLength);
     }
 
     private int size;
