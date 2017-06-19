0627e4b8888618ea6c811137b17bde2c1e1b53de
Bencodev <benoit.cotte@essec.edu>
2017-06-19 21:57:53 +0900

    refactor humanUser play method

--- HumanUser.java
+++ HumanUser.java
@@ -1,41 +1,18 @@
-import java.util.Scanner;
-
 public class HumanUser extends User {
 
     public Position[] play() throws Exception {
         // Turn for the Human user
         // Aggregation ?? User has a board, or a board has a User ==> more User has a board
-
         Position[] lastPositions = new Position[1];
 
-        Scanner sc1 = new Scanner(System.in);
-        int abs = sc1.nextInt();
-        Scanner sc2 = new Scanner(System.in);
-        int ord = sc2.nextInt();
+        Square originalSquare = this.getBoard().promptSquare();
+        this.getBoard().unsetSquare(originalSquare.getPosition());
 
-        Scanner sc3 = new Scanner(System.in);
-        int absTarget = sc3.nextInt();
-        Scanner sc4 = new Scanner(System.in);
-        int ordTarget = sc4.nextInt();
+        Position targetPosition = this.getBoard().promptPosition();
+        this.getBoard().setSquare(targetPosition, originalSquare.getColor());
 
-        Position position = new Position(abs, ord);
-        Position targetPosition = new Position(absTarget, ordTarget);
-
-        if (this.getBoard().getSquare(position) != null) {
-            if (this.getBoard().getSquare(targetPosition) == null){
-                this.getBoard().setSquare(targetPosition, this.getBoard().getSquare(position).getColor());
-                this.getBoard().unsetSquare(position);
-                lastPositions[0] = targetPosition;
-            }
-            else {
-                // TODO raise exception
-            }
-        }
-        else {
-            // TODO raise exception nothing there
-        }
+        lastPositions[0] = targetPosition;
 
         return lastPositions;
     }
-
 }
