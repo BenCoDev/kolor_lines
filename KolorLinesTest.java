b4a62720dba14427553e08eccf61154db22d29bc
Bencodev <benoit.cotte@essec.edu>
2017-06-19 18:00:21 +0900

    cover processPositions

--- KolorLinesTest.java
+++ KolorLinesTest.java
@@ -1,7 +1,36 @@
-package PACKAGE_NAME;
+import org.junit.Test;
 
-/**
- * Created by benoitcotte on 19/06/2017.
- */
+import java.util.LinkedList;
+
+import static org.junit.Assert.*;
+
 public class KolorLinesTest {
+
+    @Test
+    public void processPosition() throws Exception {
+        int SIZE = 3;
+        Board board = new Board(SIZE);
+
+        // System placed 3 squares valid for alignment
+        String[] boardSerialized = {
+                null, "RAINBOW", null,
+                null, "RAINBOW", null,
+                "BLEU", "BLEU", "BLEU"
+        };
+
+        board.load(boardSerialized);
+
+        // System positions
+        Position[] positions = new Position[3];
+        positions[0] = new Position(1, 0);
+        positions[1] = new Position(1, 1);
+        positions[2] = new Position(1, 2);
+
+        LinkedList<LinkedList<Square>> validAlignments = KolorLines.processPositions(board, positions);
+        assertEquals("Size should be 2 - avoid duplicate counting", validAlignments.size(), 2);
+
+
+
+    }
+
 }
