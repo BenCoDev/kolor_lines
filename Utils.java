b4f4579d531fffb6f31edfe4a026c79ee36e38f9
Bencodev <benoit.cotte@essec.edu>
2017-06-19 22:00:33 +0900

    add promptInt in Utils

--- Utils.java
+++ Utils.java
@@ -1,7 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created by benoitcotte on 19/06/2017.
  */
 public class Utils {
+    public static int promptInt(String valueName){
+        System.out.println("Enter " + valueName);
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int abs = scanner.nextInt();
+            return abs;
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("You should enter an integer");
+            return promptInt(valueName);
+        }
+    }
+
 }
